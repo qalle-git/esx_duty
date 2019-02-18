2fc2fe804aeab35d78c6f4d830a0413d0e8aad37
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-02-18 16:19:59 -0500

    added Sandy Shore Police Station

--- config.lua
+++ config.lua
@@ -11,6 +11,13 @@ Config.Zones = {
     Color = { r = 0, g = 255, b = 0 },  
     Type  = 27,
   },
+  
+  PoliceSandyDuty = {
+    Pos   = { x = 439.825, y = -975.693, z = 29.691 },
+    Size  = { x = 2.5, y = 2.5, z = 1.5 },
+    Color = { r = 0, g = 255, b = 0 },  
+    Type  = 27,
+  },
 
   AmbulanceDuty = {
     Pos = { x = 264.45, y = -1356.84, z = 23.56 },
