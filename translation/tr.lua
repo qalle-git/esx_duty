6e657ec9d2cef299dabc37eeeebff0fa48b9ad84
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-08-02 10:28:49 +0300

    Create tr.lua

---
+++ translation/tr.lua
@@ -0,0 +1,7 @@
+Locales['tr'] = { 
+  ['duty'] = '~INPUT_CONTEXT~ tuşuna basarak göreve ~g~başlayabilir~s~/~r~çıkabilirsin~s~',
+	['onduty'] = 'Görev başındasın.',
+	['offduty'] = 'Görev başında değilsin.',
+	['notpol'] = 'İşe başlamak için polis olman gerekiyor..',
+	['notamb'] = 'İşe başlamak için doktor olman gerekiyor..',
+}
