fed130af321b6a93c1d647b386843118ae2dc5fe
Demo <51279632+Demo4889@users.noreply.github.com>
2020-05-24 23:42:12 -0400

    Update en.lua
    Fixed some spelling and grammar issues.

--- translation/en.lua
+++ translation/en.lua
@@ -1,8 +1,8 @@
 Locales['en'] = {
   -- regulars
-  	['duty'] = 'Press ~INPUT_CONTEXT~ to ~g~emter~s~/~r~exit~s~ duty',
+  	['duty'] = 'Press ~INPUT_CONTEXT~ to ~g~enter~s~/~r~exit~s~ duty',
 	['onduty'] = 'You went onduty.',
 	['offduty'] = 'You went offduty.',
-	['notpol'] = 'You are not a policemen.',
-	['notamb'] = 'You are not a doctor.',
+	['notpol'] = 'You are not a police officer.',
+	['notamb'] = 'You are not ems.',
 }
