7080d9b65a550b29465faa3e31bbab3e4bffd111
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-07 01:06:23 +0100

    Update en.lua

--- translation/en.lua
+++ translation/en.lua
@@ -1,8 +1,8 @@
 Locales['en'] = {
   -- regulars
-  	['duty'] = 'Press ~INPUT_CONTEXT~ to ~g~emter~s~/~r~exit~s~ duty',
-	['onduty'] = 'You went onduty.',
-	['offduty'] = 'You went offduty.',
-	['notpol'] = 'You are not a policemen.',
-	['notamb'] = 'You are not a doctor.',
+  	['duty'] = 'Press ~INPUT_CONTEXT~ to ~g~enter~s~/~r~exit~s~ duty',
+	['onduty'] = 'You went on duty.',
+	['offduty'] = 'You went off duty.',
+	['notpol'] = 'You are not an officer.',
+	['notamb'] = 'You are not an emt.',
 }
